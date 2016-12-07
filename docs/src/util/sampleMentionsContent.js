ff32b853804872692d2426f77a335c6c274204e3
Andrés Lucas Enciso <andresin87@gmail.com>
2016-12-07 23:25:59 +0100

    ADD utils

---
+++ docs/src/util/sampleMentionsContent.js
@@ -0,0 +1,122 @@
+export const sampleMentionsArray = [
+  {
+    name: {
+      first: 'necati',
+      last: 'balcı',
+    },
+  },
+  {
+    name: {
+      first: 'lilja',
+      last: 'manni',
+    },
+  },
+  {
+    name: {
+      first: 'lucile',
+      last: 'schmitt',
+    },
+  },
+  {
+    name: {
+      first: 'susan',
+      last: 'thompson',
+    },
+  },
+  {
+    name: {
+      first: 'alexandros',
+      last: 'hunneman',
+    },
+  },
+  {
+    name: {
+      first: 'davut',
+      last: 'akyüz',
+    },
+  },
+  {
+    name: {
+      first: 'eugenia',
+      last: 'hidalgo',
+    },
+  },
+  {
+    name: {
+      first: 'rense',
+      last: 'van lith',
+    },
+  },
+  {
+    name: {
+      first: 'emma',
+      last: 'ambrose',
+    },
+  },
+  {
+    name: {
+      first: 'jacob',
+      last: 'almeida',
+    },
+  },
+  {
+    name: {
+      first: 'jade',
+      last: 'scott',
+    },
+  },
+  {
+    name: {
+      first: 'david',
+      last: 'adam',
+    },
+  },
+  {
+    name: {
+      first: 'charlotte',
+      last: 'williams',
+    },
+  },
+  {
+    name: {
+      first: 'lola',
+      last: 'coleman',
+    },
+  },
+  {
+    name: {
+      first: 'begüm',
+      last: 'beşok',
+    },
+  },
+  {
+    name: {
+      first: 'hudson',
+      last: 'fortin',
+    },
+  },
+  {
+    name: {
+      first: 'christine',
+      last: 'griffin',
+    },
+  },
+  {
+    name: {
+      first: 'rosalva',
+      last: 'nunes',
+    },
+  },
+  {
+    name: {
+      first: 'rima',
+      last: 'coumans',
+    },
+  },
+  {
+    name: {
+      first: 'karen',
+      last: 'sutton',
+    },
+  },
+];
